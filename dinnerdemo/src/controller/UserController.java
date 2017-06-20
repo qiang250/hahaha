57c5f5cee9274a7742cc5be7c148bc4dca739ee1
wwq <673341695@qq.com>
2017-06-20 11:56:15 +0800

    test

--- dinnerdemo/src/controller/UserController.java
+++ dinnerdemo/src/controller/UserController.java
@@ -2,6 +2,7 @@ package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,8 +14,8 @@ public class UserController {
 	@Autowired
 	public UserService userService;
 	@RequestMapping(value="UserRegister", method=RequestMethod.POST)
-	public String UserRegister(User user){
-		System.out.println(user.getPassword());
+	public String UserRegister(Model model){
+		System.out.println(((User) model).getPassword());
 		return "login";
 
 		}
